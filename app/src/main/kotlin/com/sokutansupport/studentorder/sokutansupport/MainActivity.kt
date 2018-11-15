c41752246e520134cd3f77f3c784a77bcfe88ccd
naomath <math1972naonao@gmail.com>
2018-11-15 21:13:40 +0900

    next design main.xml

--- app/src/main/kotlin/com/sokutansupport/studentorder/sokutansupport/MainActivity.kt
+++ app/src/main/kotlin/com/sokutansupport/studentorder/sokutansupport/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.sokutansupport.studentorder.sokutansupport
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+//for the above import sentence, i can use that's id as an object below without declaring
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_today -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_calendar -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_list -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
