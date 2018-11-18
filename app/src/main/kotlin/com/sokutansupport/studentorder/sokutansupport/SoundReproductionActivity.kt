e20b031ad4cfea3cab72948ea7963b7d2d017b5d
naomath <math1972naonao@gmail.com>
2018-11-18 14:10:37 +0900

    anyway

---
+++ app/src/main/kotlin/com/sokutansupport/studentorder/sokutansupport/SoundReproductionActivity.kt
@@ -0,0 +1,12 @@
+package com.sokutansupport.studentorder.sokutansupport
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SoundReproductionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sound_reproduction)
+    }
+}
--- app/src/main/kotlin/com/sokutansupport/studentorder/sokutansupport/fragments/TodayFragment.kt
+++ app/src/main/kotlin/com/sokutansupport/studentorder/sokutansupport/fragments/TodayFragment.kt
@@ -1,6 +1,5 @@
 package com.sokutansupport.studentorder.sokutansupport.fragments
 
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -9,27 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.sokutansupport.studentorder.sokutansupport.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [TodayFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [TodayFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class TodayFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
 
     companion object {
 
@@ -40,50 +19,17 @@ class TodayFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_today, container, false)
+        val view = inflater.inflate(R.layout.fragment_today, container, false)
+        return view
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
     interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
     }
 
